b904f7d8cc159b2c6d2fa9bea5ce8185e4045730
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-10-08 16:43:29 +0300

    add "Register" component

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Home from '@/views/Home';
+import Register from '@/views/Register';
 
 const routes = [
   {
@@ -8,13 +9,11 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+
 ]
 
 const router = createRouter({
